a706508a2db7254990322847ff762a054f764e0a
lemin194 <leminhduc190204@gmail.com>
2023-09-29 23:43:11 +0700

    first commit

---
+++ nni_test/main.py
@@ -0,0 +1,199 @@
+from pathlib import Path
+
+import nni.nas.strategy as strategy
+import torchvision
+from nni.nas.evaluator import FunctionalEvaluator
+from nni.nas.experiment import NasExperiment, NasExperimentConfig
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+import torch.backends.cudnn as cudnn
+import random
+import torch
+import torch.nn as nn
+
+from torchvision.models import VGG
+
+import os
+import argparse
+import logging
+
+
+import nni
+
+
+import model_builder
+from utils import *
+
+
+trainloader = None
+testloader = None
+net = None
+criterion = None
+optimizer = None
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0.0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+
+_logger = logging.getLogger("cifar10_pytorch_automl")
+
+trainloader = None
+testloader = None
+net = None
+criterion = None
+optimizer = None
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0.0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+def prepare(args):
+    global trainloader
+    global testloader
+    global net
+    global criterion
+    global optimizer
+
+
+
+    # Data
+    print('==> Preparing data..')
+
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.Resize(32),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.Resize(32),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = CIFAR10(root='../data/cifar10', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=1)
+
+    testset = CIFAR10(root='../data/cifar10', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=1)
+
+    # Model
+    print('==> Building model..')
+    net, optimizer = model_builder.build_model(args)
+
+    net = net.to(device)
+
+    # if device == 'cuda':
+    #     net = torch.nn.DataParallel(net)
+    #     cudnn.benchmark = True
+
+    criterion = nn.CrossEntropyLoss()
+
+
+# Training
+def train(epoch, batches=-1):
+    global trainloader
+    global testloader
+    global net
+    global criterion
+    global optimizer
+
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        acc = 100.*correct/total
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+        if batches > 0 and (batch_idx+1) >= batches:
+            return
+
+def test(epoch):
+    global best_acc
+    global trainloader
+    global testloader
+    global net
+    global criterion
+    global optimizer
+
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            acc = 100.*correct/total
+
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt.t7')
+        best_acc = acc
+    return acc, best_acc
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=200)
+
+    # Maximum mini-batches per epoch, for code testing purpose
+    parser.add_argument("--batches", type=int, default=-1)
+
+    args, _ = parser.parse_known_args()
+
+    print('running')
+    try:
+        RCV_CONFIG = nni.get_next_parameter()
+        #RCV_CONFIG = {'lr': 0.1, 'optimizer': 'Adam', 'model':'senet18'}
+        _logger.debug(RCV_CONFIG)
+
+        prepare(RCV_CONFIG)
+        acc = 0.0
+        best_acc = 0.0
+        for epoch in range(start_epoch, start_epoch+args.epochs):
+            train(epoch, args.batches)
+            acc, best_acc = test(epoch)
+            nni.report_intermediate_result(acc)
+
+        nni.report_final_result(best_acc)
+    except Exception as exception:
+        _logger.exception(exception)
+        raise
---
+++ nni_test/model_builder.py
@@ -0,0 +1,66 @@
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import torch.optim as optim
+
+class DepthwiseSeparableConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, bias=False):
+        super(DepthwiseSeparableConv2d, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size,
+                                   groups=in_channels, bias=bias, padding=(kernel_size - 1) // 2)
+        self.pointwise = nn.Conv2d(in_channels, out_channels,
+                                   kernel_size=1, bias=bias)
+
+    def forward(self, x):
+        out = self.pointwise(self.depthwise(x))
+        return out
+
+def build_model(args):
+    input_dim = (1, 3, 32, 32)
+    output_size = 10
+
+    model = nn.Sequential()
+
+    conv1 = nn.Conv2d(3, 32, 3, stride=1, padding=1)
+    conv2 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
+
+    if (args['conv2'] == 'normal'):
+        conv2 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
+    elif (args['conv2'] == 'depthwise'):
+        conv2 = DepthwiseSeparableConv2d(32, 64)
+
+    dropout = nn.Dropout(args['dropout_rate'])
+
+    model.add_module('conv1', conv1)
+    model.add_module('relu1', nn.ReLU())
+    model.add_module('conv2', conv2)
+    model.add_module('relu2', nn.ReLU())
+    model.add_module('dropout', dropout)
+
+    conv_out_size = np.prod(model(torch.zeros((1, *input_dim[1:]))))
+
+    fc1 = nn.Linear(conv_out_size, args['fc1'])
+    fc2 = nn.Linear(args['fc1'], output_size)
+
+    model.add_module('fc1', fc1)
+    model.add_module('relu3', nn.ReLU())
+    model.add_module('fc2', fc2)
+    model.add_module('output', nn.Softmax(dim=1))
+
+    lr = args.setdefault("lr", 1e-3)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    if args['optimizer'] == 'SGD':
+        optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
+    if args['optimizer'] == 'Adadelta':
+        optimizer = optim.Adadelta(model.parameters(), lr=lr)
+    if args['optimizer'] == 'Adagrad':
+        optimizer = optim.Adagrad(model.parameters(), lr=lr)
+    if args['optimizer'] == 'Adam':
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+    if args['optimizer'] == 'Adamax':
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+    return model, optimizer
+
